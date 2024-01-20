ef02ee1366e4cec39d887047fb8fab213288b4bc
Subin Han <subinhan0803@gmail.com>
2024-01-20 10:45:27 -0600

    added floor

--- bouncy_bombs.py
+++ bouncy_bombs.py
@@ -1,31 +1,50 @@
-import pymunk               # Import pymunk
+#Import libraries
+import pymunk
 import pygame
 
 
 pygame.init()
-display = pygame.display.set_mode((1000, 1000))
+window_size = (800, 800)
+display = pygame.display.set_mode(window_size)
 clock = pygame.time.Clock()
 FPS = 50
-space = pymunk.Space()      # Create a Space which contain the simulation
-space.gravity = 0,981      # Set its gravity
 
-body = pymunk.Body()        # Create a Body
-body.position = 500, 500      # Set the position of the body
+# Create a Space which contain the simulation
+space = pymunk.Space()
 
-shape = pymunk.Circle(body, 10) # Create a circle shape and attach to body
-shape.mass = 10              # Set the mass on the shape
+# Set its gravity
+space.gravity = 0,981
+
+# Create a Body
+bomb = pymunk.Body()
+# Set the position of the body
+bomb.position = window_size[0]/2, window_size[1]/2
+
+# Create a circle shape and attach to body
+shape = pymunk.Circle(bomb, 10) 
+# Set the mass and density on the shape
+shape.mass = 10
 shape.density = 1
-space.add(body, shape)       # Add both body and shape to the simulation
+# Add both body and shape to the simulation
+space.add(bomb, shape)
+
+# Create floor
+floor = pymunk.Body(body_type=pymunk.Body.STATIC)
+floor.shape = pymunk.Segment(floor, (0, 750), (800, 750), 50)
+#floor.position = window_size[0]/2, window_size[1]
+space.add(floor)
 
 
+#Create game loop
 def game():
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         display.fill((255, 255, 255))
-        x, y = body.position
+        x, y = bomb.position
         pygame.draw.circle(display, (255, 0, 0), (int(x), int(y)), 10)
+        #pygame.draw.rect(display, (128, 128, 128), (0, window_size[0]-50, window_size[0], 100))
         pygame.display.update()
         clock.tick(FPS)
         space.step(1/FPS)
